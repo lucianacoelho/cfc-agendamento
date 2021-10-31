ec32310f180d7346d8f725fe47fa1b360fde91db
Leonardo Ferreira <70867390+LeoACF@users.noreply.github.com>
2021-10-31 16:16:24 -0300

    Update add.cor.php

--- add.cor.php
+++ add.cor.php
@@ -4,11 +4,11 @@
  
  if (!$con)
  {
-	 echo 'not connected to server';
+	 echo 'Não foi possível conectar ao servidor';
  }
  if (!mysqli_select_db($con, 'b7_30185286_db'))
  {
-	 echo 'database not selected';
+	 echo 'Banco de dados não selecionado';
  }
 
  $Course_Code = $_POST['corcode'];
@@ -18,7 +18,7 @@
 
  if (!mysqli_query ($con, $sql))
  {
-	 echo 'not inserted';
+	 echo 'Não inserido';
  }
  else 
  {
